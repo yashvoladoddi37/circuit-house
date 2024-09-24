e9a44a42d8b425961fa44fd3d6b776fce2af6cdf
yashvoladoddi37 <89261520+yashvoladoddi37@users.noreply.github.com>
2024-09-24 06:51:40 +0530

    added basic limit-offset pagination AND content fetching according to content providers

--- content-recommendation-poc/app/movie_based_reco.py
+++ content-recommendation-poc/app/movie_based_reco.py
@@ -2,40 +2,40 @@ import pandas as pd
 from scipy import stats
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from app.utils import load_data
+from enum import Enum
 
-# Load the dataset
-def load_data(file_path):
-    return pd.read_csv(file_path)
+# Define an Enum for content providers
+class ProviderEnum(str, Enum):
+    zee5 = "Zee5"
+    magellan = "Magellan TV"
+    both = "Both"
 
 # Parse and clean the 'genres' column
 def split_genres(genre_str):
-    if pd.isna(genre_str):  # Handle missing values
-        return []
-    return [genre.strip() for genre in genre_str.split(',')]
-
-def split_keywords(keyword_str):
-    if pd.isna(keyword_str):  # Handle missing values
-        return []
-    return [key.strip() for key in keyword_str.split(',')]
+    genres = []
+    for part in genre_str.split(','):
+        genres.extend(part.split('&'))
+    return [genre.strip() for genre in genres]
 
 # Extract and process features from the data
 def process_data(md):
+    md['genres'] = md['genres'].apply(lambda x: ' '.join(x))
     md['tagline'] = md['tagline'].fillna('')
     md['description'] = md['description'].fillna('')
-    # Don't join genres here, keep as a list
-    md['keywords'] = md['keywords'].apply(lambda x: ' '.join(x) if isinstance(x, list) else '')
-    md['overview'] = md['description'] + ' ' + md['tagline'] + ' ' + md['genres'].apply(lambda x: ' '.join(x) if isinstance(x, list) else '') + ' ' + md['keywords']
+    md['overview'] =  md['description'] + md['tagline'] + md['genres']
+
     return md
 
 # Calculate cosine similarity
 def calculate_cosine_similarity(smd):
     tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), min_df=0.0, stop_words='english')
-    tfidf_matrix = tf.fit_transform(smd['overview'])
+    tfidf_matrix = tf.fit_transform(smd['description'])
     cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
     return cosine_sim
 
 # Get movie recommendations
-def improved_recommendations(title, smd, indices, cosine_sim):
+def improved_recommendations(title, smd, indices, cosine_sim, providers=None):
     if title not in indices:
         return "Movie not in database, search for another movie."
 
@@ -46,20 +46,30 @@ def improved_recommendations(title, smd, indices, cosine_sim):
     movie_indices = [i[0] for i in sim_scores]
 
     movies = smd.iloc[movie_indices]
-    vote_counts = movies[movies['vote_count'].notnull()]['vote_count'].astype('int')
-    vote_averages = movies[movies['vote_average'].notnull()]['vote_average'].astype('int')
+    
+    # Filter by provider if specified
+    if providers:
+        if providers == "Both":
+            # Include both providers
+            movies = movies[movies['provider'].isin(["Zee5", "Magellan TV"])]
+        else:
+            movies = movies[movies['provider'] == providers]
+
+    movies['vote_count'] = pd.to_numeric(movies['vote_count'], errors='coerce')
+    movies['vote_average'] = pd.to_numeric(movies['vote_average'], errors='coerce')
+    movies = movies.dropna(subset=['vote_count', 'vote_average'])
+    vote_counts = movies['vote_count'].astype('int')
+    vote_averages = movies['vote_average'].astype('int')
     C = vote_averages.mean()
     m = vote_counts.quantile(0.60)
     qualified = movies[(movies['vote_count'] >= m) & (movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
     qualified['vote_count'] = qualified['vote_count'].astype('int')
     qualified['vote_average'] = qualified['vote_average'].astype('int')
     qualified['wr'] = qualified.apply(lambda x: weighted_rating(x, m, C), axis=1)
-    qualified = qualified.sort_values('wr', ascending=False).head(10)
-    
-    # Convert genres list to comma-separated string
-    qualified['genres'] = qualified['genres'].apply(lambda x: ', '.join(x) if isinstance(x, list) else x)
+    qualified = qualified.sort_values('wr', ascending=False).head(30)
+    qualified['genres'] = qualified['genres'].apply(lambda x: ', '.join(x.split()))
 
-    return qualified[['id', 'title', 'content_type', 'genres', 'backdrop_path', 'poster_path']]
+    return qualified[['id', 'title', 'content_type', 'genres', 'backdrop_path','poster_path', 'provider']]
 
 # Weighted rating formula
 def weighted_rating(x, m, C):
@@ -68,22 +78,17 @@ def weighted_rating(x, m, C):
     return (v / (v + m) * R) + (m / (m + v) * C)
 
 # Main function
-def movie_based_movies(title: str, limit: int):
-    file_path = './datasets/enriched_ch_data_new.csv'
-    md = load_data(file_path)
+def movie_based_movies(title: str, limit: int, providers: str = None):  # Changed providers to str
+    bucket_name = 'enriched-dataset'  # Replace with your S3 bucket name
+    file_key = 'enriched_ch_data_shows.csv'  # Replace with your S3 file key
+    md = load_data(bucket_name, file_key)
+    md = md[md['content_type']=='movie']
     md['genres'] = md['genres'].fillna('').apply(split_genres)
-    md['keywords'] = md['keywords'].fillna('').apply(split_keywords)
-
     smd = process_data(md)
     cosine_sim = calculate_cosine_similarity(smd)
     smd = smd.reset_index()
     titles = smd['title']
     indices = pd.Series(smd.index, index=smd['title'])
-    recommendations = improved_recommendations(title, smd, indices, cosine_sim)
-    
-    if isinstance(recommendations, str):
-        # If recommendations is a string, it means the movie was not found
-        return recommendations
-    else:
-        # If recommendations is a DataFrame, return the limited results
-        return recommendations.head(limit).reset_index(drop=True)
+    recommendations = improved_recommendations(title, smd, indices, cosine_sim, providers)  # Pass providers to the recommendations function
+    recommendations['genres'] = recommendations['genres'].fillna('').apply(split_genres)
+    return recommendations.head(limit).reset_index(drop=True)
